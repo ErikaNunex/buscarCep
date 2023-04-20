9d46276daad7d8ffe42a4073618d54062a7a0fb1
ErikaNunes <erikanunes0512@gmail.com>
2023-04-20 14:55:32 -0300

    preparando dependencia

--- README.md
+++ README.md
@@ -1,29 +1,43 @@
 # buscar-cep
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your unit tests
+
+Jest:
+
 ```
 npm run test:unit
 ```
 
+Cypress:
+
+```
+npx cypress open
+```
+
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+const vuetify = createVuetify({
+    components,
+    directives
+});
+createApp(App).use(pinia).use(vuetify).use(VueAxios,axios).use(router).mount('#app')
